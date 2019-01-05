fcf29d29e072eadc23bb2b24622169ef4b6e45b1
asarch <asarch@hotmail.com>
2019-01-04 21:20:41 -0600

    Get the object declaration from the page.

---
+++ 4chan.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Let's get the raw HTML code of the catalog
+wget -O - http://boards.4chan.org/s/catalog > catalog.html
+
+# Let's split </foo><bar>, </foo> <bar> tags and save the output to the catalog2.html file:
+ex catalog.html -c <<EOS
+:%s/></>\\
+</g
+:%s/> </>\\
+</g
+:w catalog2.html
+:q
+EOS
+
+# Let's look for the <script> tag with the "author" content and save the output to the script.html file:
+grep script catalog2.html | grep author > script.html
+
+# Let's get all the variable declaration and save it to the vars.js file:
+sed script.html -c <<EOS
+:s/;var/;\\
+var/g
+:w var.js
+EOS
+
+# Let's look for the declaration of the object catalog and save the output to catalog.js:
+grep catalog vars.js > catalog.js
